4630bdcf760aec1d0636de3ef6ea0caa0b0257b4
Globins <50190118+Globins@users.noreply.github.com>
2021-02-06 15:20:21 -0800

    Update elytra_test.py

--- elytra_test.py
+++ elytra_test.py
@@ -1,15 +1,16 @@
+from __future__ import print_function
 # ------------------------------------------------------------------------------------------------
 # Copyright (c) 2016 Microsoft Corporation
-#
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 # associated documentation files (the "Software"), to deal in the Software without restriction,
 # including without limitation the rights to use, copy, modify, merge, publish, distribute,
 # sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
+# 
 # The above copyright notice and this permission notice shall be included in all copies or
 # substantial portions of the Software.
-#
+# 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
 # NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -17,69 +18,61 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # ------------------------------------------------------------------------------------------------
 
-# Tutorial sample #7: The Maze Decorator
-
-try:
-    from malmo import MalmoPython
-except:
-    import MalmoPython
-
+from builtins import range
+import MalmoPython
 import os
+import random
 import sys
 import time
 import json
+import random
+import errno
+import malmoutils
 import pyautogui
 import pygetwindow as gw
 
-# sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-def initialize_inventory():
-    inventoryxml = "<Inventory>"
-    for i in range(0,36):
-      inventoryxml += "<InventoryItem slot='{}' type='fireworks' quantity='64'/>".format(i)
-    inventoryxml += "<InventoryItem slot='38' type='elytra'/>"
-    inventoryxml += "</Inventory>"
-    return inventoryxml
+malmoutils.fix_print()
 
-def GetMissionXML(seed, gp, size=10):
-    return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+agent_host = MalmoPython.AgentHost()
+malmoutils.parse_command_line(agent_host)
+recordingsDirectory = malmoutils.get_recordings_directory(agent_host)
+video_requirements = '<VideoProducer><Width>860</Width><Height>480</Height></VideoProducer>' if agent_host.receivedArgument("record_video") else ''
+observation_size = 10
 
-              <About>
-                <Summary>Hello world!</Summary>
-              </About>
+items=["red_flower white_tulip", "coal", "planks spruce", "planks birch", "planks dark_oak", "rabbit", "carrot", "potato", "brown_mushroom"]
 
-            <ServerSection>
-              <ServerInitialConditions>
-                <Time>
-                    <StartTime>1000</StartTime>
-                    <AllowPassageOfTime>false</AllowPassageOfTime>
-                </Time>
-                <Weather>clear</Weather>
-              </ServerInitialConditions>
-              <ServerHandlers>
-                  <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"/>
-                  <DrawingDecorator>
-                    <DrawSphere x="0" y="70" z="0" radius="30" type="air"/>
-                    <DrawBlock x='0'  y='55' z='0' type='diamond_ore' />
-                  </DrawingDecorator>
-                  <ServerQuitFromTimeUp timeLimitMs="10000"/>
-                  <ServerQuitWhenAnyAgentFinishes/>
-                </ServerHandlers>
-              </ServerSection>
-
-              <AgentSection mode="Survival">
-                <Name>CS175AwesomeMazeBot</Name>
-                <AgentStart>
-                    <Placement x="0.5" y="56.0" z="0.5" yaw="0"/>
-                    '''+ initialize_inventory() + '''
-                </AgentStart>
-                <AgentHandlers>
-                    <ContinuousMovementCommands/>
-                </AgentHandlers>
-              </AgentSection>
-            </Mission>'''
+def buildPositionList(items):
+    positions=[]
+    for item in items:
+        positions.append((random.randint(-10,10), random.randint(-10,10)))
+    return positions
+def getSubgoalPositions(positions):
+    goals=""
+    for p in positions:
+        goals += '<Point x="' + str(p[0]) + '" y="227" z="' + str(p[1]) + '" tolerance="1" description="Checkpoint" />'
+    return goals
 
 
+
+def initialize_inventory(agent_host):
+  for i in range(0, 36):
+    agent_host.sendCommand("chat /give @p fireworks 64 0 {Fireworks:{Flight:1}}")
+  agent_host.sendCommand("chat /gamemode 0")
+#
+def boost(obs, agent_host):
+  checkRocketPosition(obs, agent_host)
+  agent_host.sendCommand("use 1")
+
+def checkRocketPosition(obs, agent_host):
+    '''Make sure our rockets, if we have any, is in slot 0.'''
+    for i in range(1,39):
+        key = 'InventorySlot_'+str(i)+'_item'
+        if key in obs:
+            item = obs[key]
+=
+            if item == 'fireworks':
+                agent_host.sendCommand("swapInventoryItems 0 " + str(i))
+                return
 def launch(agent_host):
   minecraftWin = gw.getWindowsWithTitle('Minecraft 1.11.2')[0]
   minecraftWin.activate()
@@ -91,75 +84,136 @@ def launch(agent_host):
   time.sleep(.15)
   pyautogui.keyUp('space')
   pyautogui.press('enter')
-             
-
-def boost(agent_host):
-  #SWAP ATTACK AND USE IN MINECRAFT OPTIONS TO USE
-  #check if empty, if so
-  #swap with next slot
-  agent_host.sendCommand("use 1")
 
 
-# Create default Malmo objects:
-agent_host = MalmoPython.AgentHost()
-try:
-    agent_host.parse( sys.argv )
-except RuntimeError as e:
-    print('ERROR:',e)
-    print(agent_host.getUsage())
-    exit(1)
-if agent_host.receivedArgument("help"):
-    print(agent_host.getUsage())
-    exit(0)
+def printInventory(obs):
+    for i in range(0,9):
+        key = 'InventorySlot_'+str(i)+'_item'
+        var_key = 'InventorySlot_'+str(i)+'_variant'
+        col_key = 'InventorySlot_'+str(i)+'_colour'
+        if key in obs:
+            item = obs[key]
+            print(str(i) + " ------ " + item, end=' ')
+        else:
+            print(str(i) + " -- ", end=' ')
+        if var_key in obs:
+            print(obs[var_key], end=' ')
+        if col_key in obs:
+            print(obs[col_key], end=' ')
+        print()
+
+def GetMissionXML(summary):
+    ''' Build an XML mission string that uses the RewardForCollectingItem mission handler.'''
+    
+    positions = buildPositionList(items)
+    
+    return '''<?xml version="1.0" encoding="UTF-8" ?>
+    <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <About>
+            <Summary>''' + summary + '''</Summary>
+        </About>
+
+        <ServerSection>
+            <ServerHandlers>
+                <FlatWorldGenerator generatorString="3;7,2*3,2;1;" />
+                <DrawingDecorator>
+                    <DrawCuboid x1="-50" y1="4" z1="-50" x2="50" y2="3" z2="50" type="air" />   <!-- to clear old items-->
+                    <DrawBlock x='0'  y='50' z='0' type='diamond_ore' />
+                    <DrawEntity x="0" y="4" z="0" type="Cow" yaw="0"/>
+                </DrawingDecorator>
+                <ServerQuitFromTimeUp timeLimitMs="150000"/>
+                <ServerQuitWhenAnyAgentFinishes />
+            </ServerHandlers>
+        </ServerSection>
+
+        <AgentSection mode="Creative">
+            <Name>Wright</Name>
+            <AgentStart>
+                <Placement x="0.5" y="52.0" z="0.5"/>
+                <Inventory>
+                  <InventoryItem slot='38' type='elytra'/>
+                </Inventory>
+            </AgentStart>
+            <AgentHandlers>
+                <ContinuousMovementCommands turnSpeedDegs="480"/>
+                <ChatCommands/>
+                <ObservationFromSubgoalPositionList>''' + getSubgoalPositions(positions) + '''
+                </ObservationFromSubgoalPositionList>
+                <ObservationFromFullInventory/>
+                ''' + video_requirements + '''
+            </AgentHandlers>
+        </AgentSection>
+
+    </Mission>'''
+
+
+
+# Create a pool of Minecraft Mod clients.
+# By default, mods will choose consecutive mission control ports, starting at 10000,
+# so running four mods locally should produce the following pool by default (assuming nothing else
+# is using these ports):
+validate = True
+my_client_pool = MalmoPython.ClientPool()
+my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10000))
+my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10001))
+my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10002))
+my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10003))
 
 if agent_host.receivedArgument("test"):
-    num_repeats = 1
+    num_reps = 1
 else:
-    num_repeats = 1
+    num_reps = 1
 
-for i in range(num_repeats):
-    size = int(6 + 0.5*i)
-    print("Size of maze:", size)
-    my_mission = MalmoPython.MissionSpec(GetMissionXML("0", 0.4 + float(i/20.0), size), True)
-    my_mission_record = MalmoPython.MissionRecordSpec()
-    my_mission.requestVideo(800, 500)
-    my_mission.setViewpoint(1)
-    # Attempt to start a mission:
+for iRepeat in range(num_reps):
+    my_mission = MalmoPython.MissionSpec(GetMissionXML("Flight #" + str(iRepeat)),validate)
+    my_mission_record = MalmoPython.MissionRecordSpec() # Records nothing by default
+    if recordingsDirectory:
+        my_mission_record.recordRewards()
+        my_mission_record.recordObservations()
+        my_mission_record.recordCommands()
+        if agent_host.receivedArgument("record_video"):
+            my_mission_record.recordMP4(24,2000000)
+        my_mission_record.setDestination(recordingsDirectory + "//" + "Mission_" + str(iRepeat + 1) + ".tgz")
+
     max_retries = 3
-    my_clients = MalmoPython.ClientPool()
-    my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
-
     for retry in range(max_retries):
         try:
-            agent_host.startMission( my_mission, my_clients, my_mission_record, 0, "%s-%d" % ('Moshe', i) )
+            # Attempt to start the mission:
+            agent_host.startMission( my_mission, my_client_pool, my_mission_record, 0, "Elytra Test" )
             break
         except RuntimeError as e:
             if retry == max_retries - 1:
-                print("Error starting mission", (i+1), ":",e)
+                print("Error starting mission",e)
+                print("Is the game running?")
                 exit(1)
             else:
                 time.sleep(2)
 
-    # Loop until mission starts:
-    print("Waiting for the mission", (i+1), "to start ",)
     world_state = agent_host.getWorldState()
     while not world_state.has_mission_begun:
-        #sys.stdout.write(".")
         time.sleep(0.1)
         world_state = agent_host.getWorldState()
-        for error in world_state.errors:
-            print("Error:",error.text)
 
-
-    print("Mission", (i+1), "running.")
+    total_reward = 0
+    # main loop:
+    print("Starting Flight")
+    initialize_inventory(agent_host)
     launch(agent_host)
     while world_state.is_mission_running:
-        boost(agent_host)
-        time.sleep(0.1)
+        if world_state.number_of_observations_since_last_state > 0:
+            msg = world_state.observations[-1].text
+            ob = json.loads(msg)
+            boost(ob, agent_host)
         world_state = agent_host.getWorldState()
-        for error in world_state.errors:
-            print("Error:",error.text)
-
-    print()
-    print("Mission", (i+1), "ended")
-    # Mission has ended.
+        
+    # mission has ended.
+    for error in world_state.errors:
+        print("Error:",error.text)
+    # if world_state.number_of_rewards_since_last_state > 0:
+    #     reward = world_state.rewards[-1].getValue()
+    #     print("Final reward: " + str(reward))
+    #     total_reward += reward
+    # print("Total Reward: " + str(total_reward))
+    # if total_reward < expected_reward:  # reward may be greater than expected due to items not getting cleared between runs
+    #     print("Total reward did not match up to expected reward - did the crafting work?")
+    time.sleep(0.5) # Give the mod a little time to prepare for the next mission.
